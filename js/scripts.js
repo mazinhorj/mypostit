f23949dc3b2b9bd73947d8baf610a8194af1f149
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2023-01-10 17:21:33 -0300

    done for now

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,11 @@ console.log('MazinhoBigDaddy na área!!!');
 //seletores
 
 const notesContainer = document.querySelector('#notes_container');
+const semNotas = document.querySelector('#sem_notas');
 const noteInput = document.querySelector('#note_content');
 const addNoteBtn = document.querySelector('.add_note');
+const searchInput = document.querySelector('#search_input');
+const exportBtn = document.querySelector('#export_notes')
 
 
 
@@ -12,38 +15,37 @@ const addNoteBtn = document.querySelector('.add_note');
 
 const showNotes = () => {
   const notes = getNotes();
-  console.log(notes);
-  
+
   cleanNotes();
-  
+
   notes.forEach((note) => {
-    const noteElement = createNote(note.id, note.content, note.fixed);
+    const noteElement = createNote(note.id, note.content, note.fixed, note.color);
     notesContainer.appendChild(noteElement);
   });
-
 };
 
 const cleanNotes = () => {
   notesContainer.replaceChildren([]);
 };
 
+const colorBase = "#2e2e2e";
 
 const addNote = () => {
 
   const notes = getNotes();
 
-  
+  // toggleHide();
 
   // constroi o obj nota
   const noteObj = {
     id: genId(),
     content: noteInput.value,
     fixed: false,
-    color: "red",
+    color: "",
   };
 
   // chama a função passando os parâmetros
-  const noteElement = createNote(noteObj.id, noteObj.content);
+  const noteElement = createNote(noteObj.id, noteObj.content, noteObj.color);
 
   // atribui um elemento filho (de acordo com o que foi construído na função) ao elemento pai notesContainer
   notesContainer.appendChild(noteElement);
@@ -65,6 +67,11 @@ const createNote = (id, content, fixed, color) => {
   //cria a div da nota
   const element = document.createElement('div');
   element.classList.add('note');
+  element.style.backgroundColor = color;
+  //console.log(color);
+
+
+  //console.log(color);
 
   // cria a textarea que vai o conteudo da nota
   const textarea = document.createElement('textarea');
@@ -75,22 +82,25 @@ const createNote = (id, content, fixed, color) => {
   //cria btn pin
   const pinIcon = document.createElement('i');
   pinIcon.classList.add(...['bi', 'bi-pin']);
-  pinIcon.setAttribute('aria-label', 'Fixar Nota');
+  pinIcon.setAttribute('aria-label', 'Fixar');
   element.appendChild(pinIcon);
 
   //cria btn delete
   const deleteIcon = document.createElement('i');
   deleteIcon.classList.add(...['bi', 'bi-x-lg']);
+  deleteIcon.setAttribute('aria-label', 'Excluir');
   element.appendChild(deleteIcon);
 
   //cria btn clone
   const duplicateIcon = document.createElement('i');
   duplicateIcon.classList.add(...['bi', 'bi-file-earmark-plus']);
+  duplicateIcon.setAttribute('aria-label', 'Clonar');
   element.appendChild(duplicateIcon);
-  
+
   //cria btn color
   const colorIcon = document.createElement('input');
   colorIcon.setAttribute('type', 'color');
+  colorIcon.setAttribute('aria-label', 'Trocar cor');
   element.appendChild(colorIcon);
 
   // se a nota tiver a classe fixed como true, adicone a classe correspondente
@@ -99,6 +109,12 @@ const createNote = (id, content, fixed, color) => {
   }
 
   // evento do elemento
+  element.querySelector('textarea').addEventListener('keyup', (e) => {
+    const noteContent = e.target.value;
+    updateNote(id, noteContent);
+  })
+
+
   element.querySelector('.bi-pin').addEventListener('click', () => {
     toggleFixNote(id)
   });
@@ -106,17 +122,17 @@ const createNote = (id, content, fixed, color) => {
   element.querySelector('.bi-x-lg').addEventListener('click', () => {
     deleteNote(id, element);
   });
-  
 
-  // cor
-  // const color = element.querySelector('input');
-  // const newColor = color.value
-  // color.addEventListener('click', () => {
+  element.querySelector('.bi-file-earmark-plus').addEventListener('click', () => {
+    copyNote(id);
+  });
 
-  //   console.log(newColor);
-  // });
+  element.querySelector('input').addEventListener('change', () => {
+    const newColor = element.querySelector('input').value;
+    changeColor(id, element, newColor);
+  });
 
-
+  semNotas.classList.add('esconde');
   return element;
 };
 
@@ -128,13 +144,26 @@ const toggleFixNote = (id) => {
   targetNote.fixed = !targetNote.fixed;
   saveNotes(notes);
   showNotes();
-  console.log(notes);
 };
 
-// muda a cor da nota não fixada
-const chanceColor = () => {
+const toggleHide = () => {
+  if (getNotes().length > 0) {
+    semNotas.classList.toggle('esconde');
+    return;
+  }
+};
 
-}
+
+// muda a cor da nota não fixada
+const changeColor = (id, element, newColor) => {
+  const notes = getNotes();
+  const targetNote = notes.filter((note) => note.id === id)[0];
+  element.style.backgroundColor = newColor;
+  targetNote.color = newColor;
+  saveNotes(notes);
+};
+
+
 
 // local strorage
 
@@ -156,13 +185,79 @@ const deleteNote = (id, element) => {
   notesContainer.removeChild(element)
 };
 
+const copyNote = (id) => {
+  const notes = getNotes();
+  const targetNote = notes.filter((note) => note.id === id)[0];
+  const noteObj = {
+    id: genId(),
+    content: targetNote.content,
+    fixed: false,
+    color: colorBase,
+  };
+  const noteElement = createNote(noteObj.id, noteObj.content, noteObj.fixed, noteObj.color);
+  notesContainer.appendChild(noteElement);
+  notes.push(noteObj);
+  saveNotes(notes);
+};
+
+const updateNote = (id, newContent) => {
+  const notes = getNotes();
+  const targetNote = notes.filter((note) => note.id === id)[0];
+  targetNote.content = newContent;
+  saveNotes(notes);
+};
+
+const searchNotes = (search) => {
+  const searchResults = getNotes().filter((note) => note.content.includes(search)
+  );
+  if (search !== "") {
+    cleanNotes();
+    searchResults.forEach((note) => {
+      const noteElement = createNote(note.id, note.content);
+      notesContainer.appendChild(noteElement);
+    });
+    return;
+  };
+  cleanNotes();
+  showNotes();
+};
+
+const exportData = () => {
+  const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+  const csvString = [
+    ["ID", "Conteúdo", "Fixado?"],
+    ...notes.map((note) => [note.id, note.content, note.fixed]),
+  ].map((e) => e.join(",")).join("\n");
+  const element = document.createElement('a');
+  element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+  element.target = "_blank";
+  element.download = "postits.csv";
+  element.click();
+}
 
 
 
 //eventos
-addNoteBtn.addEventListener('click', () => addNote())
+addNoteBtn.addEventListener('click', () => addNote());
+
+searchInput.addEventListener('keyup', (e) => {
+  const search = e.target.value;
+  searchNotes(search);
+});
+
+noteInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') addNote();
+});
+
+exportBtn.addEventListener('click', () => {
+  exportData();
+})
+
+
 
 
 // iniciar
+// toggleHide();
 showNotes();
 
